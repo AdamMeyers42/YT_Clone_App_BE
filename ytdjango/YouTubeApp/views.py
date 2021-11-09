9fc06e347a8fa60e6110f467c34dc0a6d220aa80
AdamMeyers42 <adammeyers3551@gmail.com>
2021-11-09 15:44:28 -0700

    Started views.py, updated settings.py for django framework

--- ytdjango/YouTubeApp/views.py
+++ ytdjango/YouTubeApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Comment
+from .serializers import CommentSerializer
+from rest_framework.views import Response 
+from rest_framework import status
+
+class 
 
 # Create your views here.
