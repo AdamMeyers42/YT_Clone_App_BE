300d5358e0587926a8a6cb7ef4d83b302781a9d3
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-10 12:30:28 -0500

    added the likes and dislikes to the RepliesApp view.py

--- ytdjango/RepliesApp/views.py
+++ ytdjango/RepliesApp/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
-from .models import Replies
-from .serializer import ReplySerializer
+from .models import Comment
+from .serializers import CommentSerializer
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.views import Response 
 from rest_framework import status
+from rest_framework.views import APIView
+from django.http.response import HttpResponse
 
 
 # Create your views here.
@@ -22,6 +24,36 @@ class RepliesList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LikeComment(APIView):
+
+    def get_id(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            return HttpResponse(status=404)
+
+    def get(self, request, pk):
+        comments = self.get_id(pk)
+        comments.like += 1
+        comments.save()
+        serializer = CommentSerializer(comments)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class DislikeComment(APIView):
+    
+    def get_id(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            return HttpResponse(status=404)
+
+    def get(self, request, pk):
+        comments = self.get_id(pk)
+        comments.dislike += 1
+        comments.save()
+        serializer = CommentSerializer(comments)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 
