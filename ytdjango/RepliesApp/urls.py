3604aa6378dcf8161e66ff4bbfc19286c4b72b39
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-10 12:31:06 -0500

    added the paths for the new classes in views.py

--- ytdjango/RepliesApp/urls.py
+++ ytdjango/RepliesApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('RepliesApp/', views.RepliesList.as_view()),
+    path('comments/like/<int:pk>/', views.LikeComment.as_view()),
+    path('comments/dislike/<int:pk>/', views.DislikeComment.as_view())
+    
 ]
 
